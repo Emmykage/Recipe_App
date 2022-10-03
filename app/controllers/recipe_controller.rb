b1b0558386b572b29d1135988a31a2569baabbb8
franclobo <fjbl2788@gmail.com>
2022-10-03 16:52:46 -0500

    Create controller for recipes with CRUD exept for Update

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -3,12 +3,19 @@ class RecipeController < ApplicationController
     @recipe = Recipe.new
   end
 
-  def def create
+  def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
       redirect_to @recipe
     else
       render 'new'
     end
+  end
+
+  def delete
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to recipes_path
   end
 end
