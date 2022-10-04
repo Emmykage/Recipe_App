905ed7a422b3ef3065998924f93f1f61befce44f
Menanya <emmiemenz@gmail.com>
2022-10-04 21:11:41 +0100

    fix rout

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "foods#index"
   resources :users, only: [:index ]
-  resources :foods, only: [:index, :new, :create]
-  resources :inventories, only: [:index, :show, :create, :new]
+  resources :foods, only: [:index, :new, :create, :destroy]
+  resources :inventories, only: [:index, :show, :create]
   resources :recipes, except: :update do
       resources :recipe_foods, only: [:create, :destroy, :edit, :new, :update]
   end
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+recipe1 = Recipe.create(name: "recipe 1", description: "for rice recipe", preparation_time: 1.5, cooking_time: 1.5, public: true, user: user_one)
