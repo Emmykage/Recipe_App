d9c6d38ab487b4c367b23733d51c1958da1fc206
Menanya <emmiemenz@gmail.com>
2022-10-04 21:55:36 +0100

    devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'inventories/index'
   # get 'inventories/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "foods#index"
   resources :users, only: [:index ]
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :inventories, only: [:index, :show, :create]
+  resources :inventories, only: [:index, :show, :new, :create]
   resources :recipes, except: :update do
       resources :recipe_foods, only: [:create, :destroy, :edit, :new, :update]
   end
   # Defines the root path route ("/")
   # root "articles#index"
 end
-
-recipe1 = Recipe.create(name: "recipe 1", description: "for rice recipe", preparation_time: 1.5, cooking_time: 1.5, public: true, user: user_one)
