565e2df74b96c18813e7c9527953e417c126c82d
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-10-05 23:16:52 +0200

    Update routes for inventory foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'publics/index'
-  # get 'inventories/index'
-  # get 'inventories/show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   root "foods#index"
   resources :users, only: [:index ]
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :inventories, only: [:index, :show, :new, :create, :destroy]
+  resources :inventories, only: [:index, :show, :new, :create, :destroy] do
+    resources :inventory_foods, only: [:create, :destroy, :new]
+  end
   resources :publics, only: [:index ]
   resources :recipes, except: :update do
       resources :recipe_foods, only: [:create, :destroy, :edit, :new, :update]
   end
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
