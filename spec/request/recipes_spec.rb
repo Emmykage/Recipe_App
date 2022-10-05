0a0af3304bc877512483decb8f38811bed9156b9
franclobo <fjbl2788@gmail.com>
2022-10-05 13:25:04 -0500

    Fix linter errors

--- spec/request/recipes_spec.rb
+++ spec/request/recipes_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'Recipes', type: :request do
   describe 'GET /recipes' do
     before :each do
       @user = User.create(name: 'Test', email: 'email@test.com', password: 'password')
-      @recipe = Recipe.create(user: @user.id, name: 'Test Recipe', description: 'Test Description', preparation_time: 60,
-                              cooking_time: 70, public: true)
+      @recipe = Recipe.create(user: @user.id, name: 'Test Recipe', description: 'Test Description',
+                              preparation_time: 60, cooking_time: 70, public: true)
     end
     it 'returns http success' do
       get recipes_path
