5cb4a305a8f189b0d748dda4b5ace0d554ec28fb
franclobo <fjbl2788@gmail.com>
2022-10-05 13:22:04 -0500

    Create tests

--- Gemfile
+++ Gemfile
@@ -51,11 +51,11 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails'
 end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem 'devise'
   gem 'letter_opener'
   gem 'web-console'
 
@@ -72,3 +72,5 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
+ gem 'devise'
---
+++ spec/request/recipes_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipes', type: :request do
+  describe 'GET /recipes' do
+    before :each do
+      @user = User.create(name: 'Test', email: 'email@test.com', password: 'password')
+      @recipe = Recipe.create(user: @user.id, name: 'Test Recipe', description: 'Test Description', preparation_time: 60,
+                              cooking_time: 70, public: true)
+    end
+    it 'returns http success' do
+      get recipes_path
+      expect(response).to have_http_status(302)
+    end
+  end
+end
