9ac5f9323000b8b652931f29d9c371e55616a0ae
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-10-06 20:05:40 +0200

    Define user abilities

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can :read, :all
+    can :manage, Recipe, author: user
+    can :manage, Inventory, author: user
+    can :manage, Public, author: user
+    return unless user.role == 'admin'
+
+    can :manage, :all
+  end
+end
